a2417be0abd0ebe00f7de172dcfa4f680afa8e2d
Bilal Malik <bilalmalik504@yahoo.com>
2023-01-14 22:07:29 +0530

    made brief projects on home. style is yet to be decided

--- src/homepage/projectonhome.js
+++ src/homepage/projectonhome.js
@@ -1,45 +1,22 @@
-import {useState} from 'react';
 import post1 from '../assets/post8.png';
 import post2 from '../assets/Post7.png';
 import post3 from '../assets/Post9.png';
+import post4 from '../assets/Post10.png';
 
 const Project = () => {
-    const [project,setProject]=useState(post1);
-    const [back1,setBack1] = useState('white');
-    const [back2,setBack2] = useState('transparent');
-    const [back3,setBack3] = useState('transparent');
-
-    const adClick1 = ()=>{
-        setProject(post1);
-        setBack1('white');
-        setBack2('transparent');
-        setBack3('transparent');
-    }
-    const adClick2 = ()=>{
-        setProject(post2);
-        setBack2('white');
-        setBack1('transparent');
-        setBack3('transparent');
-    }
-    const adClick3 = () =>{
-        setProject(post3);
-        setBack3('white');
-        setBack2('transparent');
-        setBack1('trans');
-    }
-
     return (
-        <div style={{ backgroundImage: `url(${project})`,backgroundRepeat:'no-repeat',backgroundSize:'100% 100%'}} class="projectscreen">
-            <div className="adcont">
-                <div onMouseEnter={adClick1} class="ad ad1" style={{backgroundColor:back1}}>
-
-                </div>
-                <div onMouseEnter={adClick2} className="ad ad2" style={{backgroundColor:back2}}>
-
-                </div>
-                <div onMouseEnter={adClick3} className="ad ad3" style={{backgroundColor:back3}}>
-
-                </div>
+        <div class="contpro">
+            <div class="left">
+                <h4 style={{fontSize:'32px'}}>Our Ads</h4><br/>
+            We guide game-changing companies, across platforms & places, 
+            through agile design & digital experience. We make waves<br/>
+            <a href="" class="link">Learn More</a>
+            </div>
+            <div class="right">
+                <div ><img class="box" src={post1} alt=""/></div>
+                <div ><img class="box" src={post2} alt=""/></div>
+                <div ><img class="box" src={post3} alt=""/></div>
+                <div ><img class="box" src={post4} alt=""/></div>
             </div>
         </div>
     );
